7ca251a153a7ecc2404cd3e1f5a0ea21e51109bd
Eitan Rosa <eitan.rosa@gmail.com>
2025-12-27 18:08:16 -0500

    Implement service worker lifecycle events
    Add service worker install and activate event listeners

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
 const CACHE_NAME = 'inhale-to-eat-v2-clean';
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 const ASSETS = [
   './',
   './index.html',
