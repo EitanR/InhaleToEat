19271426af69755ff621d29abfed4a04057a9627
Eitan Rosa <eitan.rosa@gmail.com>
2026-08-20 20:35:30 -0400

    Refactor service worker and update asset references
    Removed old service worker event listeners and updated asset paths.

--- sw.js
+++ sw.js
@@ -1,24 +1,12 @@
 const CACHE_NAME = 'inhale-to-eat-v1.0.5';
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
 
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
